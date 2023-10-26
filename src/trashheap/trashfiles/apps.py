d391373149f0cb59a81999c08efcc245f58f883b
James McKee <james.mckee.jr@gmail.com>
2023-10-26 11:19:24 -0500

    Reconfiguring the folder structure.

--- src/trashheap/trashfiles/apps.py
+++ src/trashheap/trashfiles/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TrashheapConfig(AppConfig):
+class TrashfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trashheap'
+    name = 'trashfiles'
