d389b4fa2b4b6ae59e5446ecbcc11fb2e3097768
James McKee <james.mckee.jr@gmail.com>
2023-10-26 22:03:38 -0500

    Updates to the admin page.

--- src/trashheap/trashfiles/admin.py
+++ src/trashheap/trashfiles/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.contrib.admin.views.main import ChangeList
+from .models import TrashFile
 
-# Register your models here.
+# Override the base admin template
+class CustomAdminSite(admin.AdminSite):
+    site_header = 'My Custom Admin Site'
+
+    def each_context(self, request):
+        context = super().each_context(request)
+        context['my_custom_var'] = 'Hello, world!'
+        return context
+
+admin_site = CustomAdminSite(name='customadmin')
+
+class TrashFileAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+    #set the plural name for this model
+    
+
+    def view_on_site(self, obj):
+        url = reverse('myapp:view_file', args=[obj.id])
+        return format_html('<a href="{}">View on site</a>', url)
+    
+    def has_add_permission(self, request):
+        return False
+
+    change_form_template = 'admin/trashfile_change_form.html'
+
+admin.site.register(TrashFile, TrashFileAdmin)
--- src/trashheap/trashfiles/models.py
+++ src/trashheap/trashfiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import hashlib
 
 # Create your models here.
+class TrashFile(models.Model):
+    # create an id field that allows for a blake3 hash
+    id = models.CharField(max_length=64, primary_key=True)
+    filenames = models.TextField()
+    filepaths = models.TextField()
+    filetypes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name_plural = "Trash Files"
+
